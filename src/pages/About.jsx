def4e6978d2a4974a4562dd0ded6dbf2167eb237
Jai Parkash <parkashj05@gmail.com>
2023-12-28 20:05:42 +0530

    chore: Fetch User Data from Database and Display in About Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Analytics from '../components/Analytics'
 import { NavLink } from 'react-router-dom'
+import { useAuth } from '../store/auth'
 
 const About = () => {
+  const {user} = useAuth();
   return (
     <>
       <main>
         <section className="section-hero">
           <div className="container grid grid-two-cols">
             <div className="hero-content">
+              <p>Welcome {user ? `${user.username} to our website` : `to our website`}</p>
               <h1>Why Choose Us? </h1>
               <p>
                 Expertise: Our team consists of experienced IT professionals who
